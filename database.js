91c80064bb5eabea31c28976495999bd32cb79d6
unknown <saxel.12194@gmail.com>
2022-09-07 19:32:34 -0500

    V4

--- database.js
+++ database.js
@@ -4,7 +4,13 @@ const mysqlConacttion = mysql.createConnection({
   host: "162.240.215.202", //Se declara el host de la bd
   user: "jeerel_userRoot", //Obtenemos el usuario al que se conectara
   password: "XML7E&)f@zx)", //Obtiene el password
+  database: "jeerel_demoLibros" //Se hace la referencia al bd
+ /* Sr Sanchez
+  host: "162.240.215.202", //Se declara el host de la bd
+  user: "jeerel_userRoot", //Obtenemos el usuario al que se conectara
+  password: "XML7E&)f@zx)", //Obtiene el password
   database: "jeerel_librodetodomexico" //Se hace la referencia al bd
+  */
   /* host: "34.67.244.163", //Se declara el host de la bd
   user: "ptree", //Obtenemos el usuario al que se conectara
   password: "Ptree2021ñ", //Obtiene el password
